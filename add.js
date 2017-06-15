184404503614a7136da0edadab9b85367d61feb7
Mikael Karon <mikael@karon.se>
2017-06-16 01:47:10 +0200

    fix: UMD fixes
    Fix UMD wrapper to for static analysis

--- add.js
+++ add.js
@@ -1,14 +1,12 @@
-(function (modules, root, factory) {
+(function (root, factory) {
   if (typeof define === "function" && define.amd) {
-    define(modules, factory);
+    define(["./capture"], factory);
   } else if (typeof module === "object" && module.exports) {
-    module.exports = factory.apply(root, modules.map(require));
+    module.exports = factory(require("./capture"));
   } else {
-    root["mu-jquery-capture/add"] = factory.apply(root, modules.map(function (m) {
-      return root[m.replace(/^\./, "mu-jquery-capture")];
-    }));
+    root["mu-jquery-capture/add"] = factory(root["mu-jquery-capture/capture"]);
   }
-})(["./capture"], this, function (capture) {
+})(this, function (capture) {
   return function ($) {
     var add = $.event.add;
     return function (elem, types, handler, data, selector) {
--- capture.js
+++ capture.js
@@ -1,14 +1,12 @@
-(function (modules, root, factory) {
+(function (root, factory) {
   if (typeof define === "function" && define.amd) {
-    define(modules, factory);
+    define([], factory);
   } else if (typeof module === "object" && module.exports) {
-    module.exports = factory.apply(root, modules.map(require));
+    module.exports = factory();
   } else {
-    root["mu-jquery-capture/capture"] = factory.apply(root, modules.map(function (m) {
-      return root[m];
-    }));
+    root["mu-jquery-capture/capture"] = factory();
   }
-})([], this, function () {
+})(this, function () {
   return function ($, fn) {
     function proxy($event) {
       var $result = $event.result;
